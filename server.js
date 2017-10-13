3ee902847d9d0ce81828555683985efd56336dc7
Egor Vodopyanov <headfire94work@gmail.com>
2017-10-13 13:03:14 +0300

    z7a

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
 const path = __dirname + '/public/';
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5007));
+
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+};
 
 app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => res.set(CORS) && next());
 
 app.get('/', function(request, response) {
+  response.sendFile(`${path}index.html`);
+});
+app.get('/xml', function(request, response) {
   response.sendFile(`${path}Task.xml`);
+});
+app.get('/author', function(request, response) {
+  response.send('Егор Водопьянов');
 });
 
 app.listen(app.get('port'), function() {
--- test.js
+++ test.js
@@ -2,16 +2,39 @@ const {spawn} = require('child_process');
 const request = require('request');
 
 // Start the app
-const env = Object.assign({}, process.env, {PORT: 5000});
+const env = Object.assign({}, process.env, {PORT: 5007});
 
 
 test('respond to request', (done) => {
   expect.assertions(1)
   const child = spawn('node', ['server.js'], {env});
   child.stdout.on('data', _ => {
-    request('http://127.0.0.1:5000', (error, response, body) => {
+    request('http://127.0.0.1:5007', (error, response, body) => {
+      expect(body.indexOf('<h4 id="author" title="GossJS">Иван Иванов</h4>')).not.toBe(-1);
+      done();
+      child.kill();
+    })
+  });
+})
+test('respond to xml request', (done) => {
+  expect.assertions(1)
+  const child = spawn('node', ['server.js'], {env});
+  child.stdout.on('data', _ => {
+    request('http://127.0.0.1:5007/xml', (error, response, body) => {
       expect(body.indexOf('<?xml')).not.toBe(-1);
       done()
+      child.kill();
+    })
+  });
+})
+test('respond to author request', (done) => {
+  expect.assertions(1)
+  const child = spawn('node', ['server.js'], {env});
+  child.stdout.on('data', _ => {
+    request('http://127.0.0.1:5007/author', (error, response, body) => {
+      expect(body).toBe('Егор Водопьянов');
+      done()
+      child.kill();
     })
   });
 })
